a4a35e0b79895cf1788430e2e212372e3102b7c6
JuanPh-02 <juanph02@outlook.com>
2026-05-23 10:49:14 -0500

    Ejercicios calculadora

---
+++ calculadora/calculadora2.js
@@ -0,0 +1,59 @@
+//let numero1 = '30';
+//let tipoDato = typeof numero1;
+
+//numero1 = Number(numero1)
+
+//console.log(tipoDato);
+
+// EJERCICIO DE CALCULADORA CON ESRUCTURA SWITCH
+
+const libreriaRead = require('readline-sync');
+
+// MENU
+console.log('APLICACION EN CONSOLA - CALCULADORA');
+console.log('Bienvenido/a hola');
+console.log('Ingrese 1 para sumar');
+console.log('Ingrese 2 para restar');
+console.log('Ingrese 3 para multiplicar');
+console.log('Ingrese 4 para dividir');
+
+let operacion = libreriaRead.questionInt('Ingrese el número de la operacion: ');
+
+if(operacion !== 1 && operacion !== 2 && operacion !== 3 && operacion !== 4 ) {
+  console.log("OPERACION NO VALIDA")
+  return;
+}
+
+let num1 = libreriaRead.questionInt('Ingrese el número 1: ');
+let num2 = libreriaRead.questionInt('Ingrese el número 2: ');
+
+let total;
+
+switch (operacion) {
+  case 1:
+    total = num1 + num2;
+    break;
+  case 2:
+    total = num1 - num2;
+    break;
+  case 3:
+    total = num1 * num2;
+    break;
+  case 4:
+    //OPCION 1
+    if (num2 !== 0) {
+      total = num1 / num2;
+    } else {
+      console.log('No se puede dividir por cero');
+    }
+
+    // OPCION 2
+    // if(num2 === 0) {
+    //   console.log("No se puede dividir por cero");
+    // } else {
+    //   console.log('Total: ', num1 / num2);
+    // }
+    break;
+}
+
+console.log('Su resultado es: ', total);
