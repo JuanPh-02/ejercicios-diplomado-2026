c2043d21b778f75196d459fcae0bc8981bd53313
JuanPh-02 <juanph02@outlook.com>
2026-06-20 15:30:11 -0500

    Api comidas

---
+++ 20Junio/ejemplo-await.js
@@ -0,0 +1,20 @@
+
+// async function obtenerRespuesta() {
+//     const response = await fetch("https://dragonball-api.com/api/characters?limit=5&page=1");
+//     console.log("Este", response)
+//     return response;
+// }
+
+
+
+// const respuesta = fetch("https://dragonball-api.com/api/characters?limit=5&page=1")
+//     .then(response => console.log(response))
+
+
+// async - define funcion asincrona
+// await - esperar 
+
+// obtenerRespuesta()
+
+// sincrono - en tiempo real
+// asincrono - en tiempos diferentes
